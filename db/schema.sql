b04c9ab90d30d022e9a72cbb60e94346ed06af8b
night <yseddiq@gmail.com>
2022-09-24 04:20:34 -0400

    finished creating tables in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,21 @@ CREATE TABLE department(
     id INTEGER PRIMARY KEY,
     name VARCHAR(30)
 )
+
+CREATE TABLE role(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+)
+
+CREATE TABLE employee(
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    department_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+)

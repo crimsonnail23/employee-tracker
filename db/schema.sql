4788ed8280b16686133eec2d91a7a3ffce8ae7dc
night <yseddiq@gmail.com>
2022-09-24 05:03:18 -0400

    created two tables, but the third keeps throwing a syntax error

--- db/schema.sql
+++ db/schema.sql
@@ -1,22 +1,24 @@
-CREATE TABLE department(
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS position;
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE department (
     id INTEGER PRIMARY KEY,
     name VARCHAR(30)
-)
+);
 
-CREATE TABLE [role](
+CREATE TABLE position (
     id INTEGER PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-)
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
 
-CREATE TABLE employee(
+CREATE TABLE employee (
     id INTEGER PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INTEGER,
-    department_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES [role](id) ON DELETE SET NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
-)
+    position_id INTEGER,
+    CONSTRAINT fk_position FOREIGN KEY (position_id) REFERENCES position(id) ON DELETE CASCADE
+);
